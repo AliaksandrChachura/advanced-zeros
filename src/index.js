de6726899be007c66e4af69752f288a1861296fc
AliaksandrChachura <skif198560@gmail.com>
2019-03-19 23:36:24 +0300

    2 mistakes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-}
+    let n, i, j, p, numberSum, noz, k;
+
+    n = number;
+    noz = n;
+    j = base;
+    for (i = 2; i <= base; i++) {
+        if (j % i === 0) {
+            p = 0;
+            while (j % i === 0) {
+                p++;
+                j /= i;
+            }
+            numberSum = 0;
+            k = i;
+            while (number  > k) {
+                numberSum += Math.floor(number / k);
+                k *= i;
+            }
+        }
+
+    }
+
+    return Math.floor(numberSum/p);
+
+};
+
